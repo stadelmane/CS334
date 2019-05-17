d3ea6ff24426709a41960fb87bb1415c9ce6b364
Eric Stadelman <stadelmane@carleton.edu>
2019-05-16 19:24:13 -0500

    final

--- bptrees/bptree.py
+++ bptrees/bptree.py
@@ -10,126 +10,85 @@ class Node(object):
 		self.internal = False
 		self.isRoot = False
 
+	'''Add value to node'''
 	def addValue(self, key, value):
 		if len(self.keys) == 0:
 			self.keys.append(key)
 			self.values.append(value)
 			return
-
 		if key in self.keys:
 			raise Exception('Key is already present. No duplicates allowed!')
-
 		for i, item in enumerate(self.keys):
-
 			if key < item:
 				self.keys.insert(i, key)
 				self.values.insert(i, value)
 				break
-
 			elif i+1 == len(self.keys):
 				self.keys.append(key)
 				self.values.append(value)
 				break
 
+	'''print method that prints the tree and leaf data'''
 	def printMethod(self, dic, level = 0):
-
-		# print(level, str(self.keys))
 		dic[level].append(str(self.keys))
-
 		if self.leaf:
 			for i in range(len(self.values)):
 				dic[level].append(str(self.values[i]))
-				# print(level, self.values[i])
-
 		if not self.leaf:
 			for item in self.values:
-				#print(item.values)
 				item.printMethod(dic, level+1)
-
 		return dic
 
-
+	'''retuns true if full'''
 	def isFull(self):
 		return (len(self.keys) == self.numKeys - 1)
 
+	'''splits a root and pulls new key up'''
 	def rootSplit(self):
 		left = Node(self.numKeys)
 		right = Node(self.numKeys)
 		middle = 0
 		if len(self.keys) % 2 == 0:
 			middle = len(self.keys) // 2
-
-			left.keys = self.keys[:middle]
-			left.values = self.values[:middle]
-			#print("left keys" , left.keys , "left values" , left.values)
-
-			right.keys = self.keys[middle:]
-			right.values = self.values[middle:]
-
-			#print("right keys" , right.keys , "right values" , right.values)
 		else:
 			middle = (len(self.keys) - 1) // 2
-			#print(middle)
-
-			left.keys = self.keys[:middle]
-			left.values = self.values[:middle]
-			#print("left keys" , left.keys , "left values" , left.values)
-
-			right.keys = self.keys[middle:]
-			right.values = self.values[middle:]
-
-			#print("right keys" , right.keys , "right values" , right.values)
+		left.keys = self.keys[:middle]
+		left.values = self.values[:middle]
+		right.keys = self.keys[middle:]
+		right.values = self.values[middle:]
 
 		self.keys = [right.keys[0]]
 		self.values = [left , right]
 		self.leaf = False
 
+	'''splits a leaf node'''
 	def leafSplit(self):
 		right = Node(self.numKeys)
 		left = Node(self.numKeys)
 		middle = 0
 		if len(self.keys) % 2 == 0:
 			middle = len(self.keys) // 2
-
-			left.keys = self.keys[:middle]
-			left.values = self.values[:middle]
-
-			right.keys = self.keys[middle:]
-			right.values = self.values[middle:]
 		else:
 			middle = (len(self.keys) - 1) // 2
-
-			left.keys = self.keys[:middle]
-			left.values = self.values[:middle]
-
-			right.keys = self.keys[middle:]
-			right.values = self.values[middle:]
-
-
+		left.keys = self.keys[:middle]
+		left.values = self.values[:middle]
+		right.keys = self.keys[middle:]
+		right.values = self.values[middle:]
 		return (right.keys[0], left, right)
 
+	'''splits an internal node'''
 	def internalSplit(self):
 		right = Node(self.numKeys)
 		left = Node(self.numKeys)
 		middle = 0
-
 		if len(self.keys) % 2 == 0:
 			middle = len(self.keys) // 2
-
-			left.keys = self.keys[:middle]
-			left.values = self.values[:middle+1]
-
-			right.keys = self.keys[middle+1:]
-			right.values = self.values[middle+1:]
 		else:
 			middle = math.ceil(len(self.keys) / 2) - 1
-
-			left.keys = self.keys[:middle]
-			left.values = self.values[:middle+1]
-
-			right.keys = self.keys[middle+1:]
-			right.values = self.values[middle+1:]
-
+		left.keys = self.keys[:middle]
+		left.values = self.values[:middle+1]
+		right.keys = self.keys[middle+1:]
+		right.values = self.values[middle+1:]
 		return (self.keys[middle], left, right)
 
 class Bptree(object):
@@ -138,7 +97,7 @@ class Bptree(object):
 		self.root = Node(self.numKeys)
 		self.root.isRoot = True
 
-
+	'''inserts values and calls root split the first time root fills up'''
 	def insert(self, key, value):
 		if self.root.leaf:
 			if self.root.isFull():
@@ -147,12 +106,12 @@ class Bptree(object):
 				return None
 		return self.insertHelper(self.root, key, value)
 
+	'''actual algorithm for inserting nodes into tree'''
 	def insertHelper(self, node, key, value):
 		if node.leaf:
 			if not node.isFull():
 				node.addValue(key, value)
 				return None
-
 			else:
 				node.addValue(key, value)
 				newKey, left, right =  node.leafSplit()
@@ -160,53 +119,40 @@ class Bptree(object):
 				node.values = left.values
 				newEntry = (newKey, right)
 				return newEntry
-
 		else:
 			child = node
 			child = self.findHelper(node, key)
-			#print(child.printMethod)
-
 			newEntry = self.insertHelper(child, key, value)
-
 			if newEntry == None:
 				return None
-
 			elif not node.isFull():
 				node.addValue(newEntry[0], newEntry[1])
 				return None
-
 			else:
-				# print(newEntry, newEntry[1].printMethod(), newEntry[1])
 				node.addValue(newEntry[0], newEntry[1])
 				k, left, right = node.internalSplit()
-				# print('impt', k, left.values[2].printMethod(), right.values[1].printMethod())
 				node.keys = left.keys
 				node.values = left.values
 				newEntry = (k , right)
 				right.leaf = False
 				right.internal = True
 				if node.isRoot:
-					# print('hi:', k, right.keys, right.values[0].values[1])
 					newRoot = Node(self.numKeys)
 					left.leaf = False
 					left.internal = True
 					node.keys = [newEntry[0]]
 					node.values = [left, right]
-					# newRoot.isRoot = True
-					# newRoot.leaf = False
-					# newRoot.addValue(k, [node, right])
 					return None
 				return newEntry
 
-
+	'''finds proper sub tree by comparing keys'''
 	def findHelper(self, node, key):
 		for i, item in enumerate(node.keys):
 			if key < item:
 				return node.values[i]
 		return node.values[i + 1]
 
-
-
+	'''recursively calls findHelper to follow proper subtree for value'''
 	def getValue(self, key):
 		node = self.root
 		while not node.leaf:
@@ -216,9 +162,10 @@ class Bptree(object):
 				return node.values[i]
 		return 'None'
 
+	'''prints entire tree'''
 	def printTree(self):
 		d = defaultdict(list)
 		objects = self.root.printMethod(d)
 		for i in range(len(d)):
-			print("Level: " , i , " " , d[i])		
-		# print(objects)
+			print("Level: " , i , " " , d[i])				
+	
--- bptrees/bptreetest.py
+++ bptrees/bptreetest.py
@@ -1,8 +1,28 @@
 import bptree
 
-b = bptree.Bptree(4)  # Each node contains 4 keys, which means 5 pointers
-b.insert(12,"hello")
-b.insert(24,"bye")
-print("Value for 12 = " + b.getValue(12))
-print("Value for 24 = " + b.getValue(24))
+b = bptree.Bptree(50)  # Each node contains 4 keys, which means 5 pointers
+b.insert(1,"hello")
+b.insert(2,"bye")
+b.insert(3,"hello")
+b.insert(4,"bye")
+b.insert(5,"hello")
+b.insert(6,"bye")
+b.insert(7,"hello")
+b.insert(8,"bye")
+b.insert(9,"hello")
+b.insert(10,"bye")
+b.insert(11,"hello")
+b.insert(12,"bye")
+b.insert(13,"hello")
+b.insert(14,"bye")
+b.insert(15,"hello")
+b.insert(16,"bye")
+b.insert(17,"hello")
+b.insert(18,"bye")
+b.insert(19,"hello")
+# b.insert(16,"bye")
+
+
+# print("Value for 12 = " + b.getValue(12))
+# print("Value for 24 = " + b.getValue(24))
 b.printTree()
